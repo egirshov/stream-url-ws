345f59a246f9181c1fde4b5803ca475c53a48416
Eugene Girshov <eugene@solu.co>
2015-11-24 09:32:47 +0200

    reimplement using websocket-stream
     * update tests & documentation
     * drop 'ws' dependency, it will be brought by 'websocket-stream'

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # WebSocket adapter for stream-url
 
-stream-url marries streams and urls. API user can start a server or a client connection using an ultra-compact universal interface of two methods:
-`listen` and `connect`.
+[stream-url](https://github.com/gritzko/stream-url) marries streams and urls. API user can start a server or a client connection using an ultra-compact universal interface of two methods: `listen` and `connect`. Underlying implementation relies on [websocket-stream](https://github.com/maxogden/websocket-stream) 
 
     // Server
 
@@ -10,7 +9,7 @@ stream-url marries streams and urls. API user can start a server or a client con
     // start a WebSocket echo server
     var server = su.listen ('ws://localhost:1234', function ready() {
 
-        server.on('connection', function (stream) {
+        server.on('stream', function (stream) {
 
             stream.on('data', stream.write.bind(stream));
 
@@ -33,3 +32,9 @@ stream-url marries streams and urls. API user can start a server or a client con
         stream.write('Hello world!');
 
     });
+
+## API
+
+Client handle is a duplex stream with additional `connect` event.
+Server is a regular websocket server with additional `stream` event which carries stream-wrapped websocket (there is still `connection` event which carries
+raw websocket object).
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-var WebSocket = require('ws');
+var websocket = require('websocket-stream');
 var url = require('url');
 var stream_url = require('stream-url');
 
@@ -7,40 +7,28 @@ var stream_url = require('stream-url');
 stream_url.register('ws:', ws_listen, ws_connect);
 //stream_url.register('wss:', ws_listen, ws_connect);
 
-function ws_listen (stream_url, callback) {
+function ws_listen (stream_url, options, callback) {
     var parsed = url.parse(stream_url);
-    var wss = new WebSocket.Server({
+    var ws_server = new websocket.Server({
         host: parsed.hostname || '127.0.0.1',
         port: parsed.port || 80,
         path: parsed.path || null
-    }, callback);
-    return wss;
+    });
+    // WebSocket.Server does not have 'listening' event
+    if (callback) {
+        process.nextTick(function () { callback(null, ws_server); });
+    }
+    return ws_server;
 }
 
-WebSocket.prototype.write = function (data, callback) {
-    this.send(data, callback);
-};
-
-WebSocket.prototype.end = function (data) {
-    this.close(1000, data);
-};
-
-WebSocket.prototype._on = WebSocket.prototype.on;
-
-WebSocket.prototype.on = function (event, callback) {
-    switch (event) {
-    case 'data':  this._on('message', callback); break;
-    case 'end':   this._on('close', callback); break;
-    default:      this._on(event, callback); break;
-    }
-};
-
-function ws_connect (stream_url, callback) {
-    var sock = new WebSocket(stream_url);
+function ws_connect (stream_url, options, callback) {
+    var stream = websocket(stream_url.href);
     if (callback) {
-        sock.on('open', callback);
+        stream.on('connect', function () {
+            callback(null, stream);
+        });
     }
-    return sock;
+    return stream;
 }
 
 module.exports = stream_url;
