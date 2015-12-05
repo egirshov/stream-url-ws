170b1d9999f4f59f7072faad4c66d39f5064a127
Victor Grishchenko <victor.grishchenko@gmail.com>
2015-12-05 10:17:01 +0300

    travis: test 5.0; adjust tests

--- test/01_serv.js
+++ test/01_serv.js
@@ -12,11 +12,12 @@ if (typeof(window)==='object') {
 tape ('1.A create echo server', function (t) {
     var port = Math.floor(Math.random()*10000) + 2000;
     var url = 'ws://localhost:'+port;
+    console.log(url);
     t.plan(4);
     var wss = su.listen(url, function ready() {
         wss.on('stream', function (stream) {
             stream.on('data', function (data) {
-                // console.log('data', data);
+                console.log('data', data);
                 stream.write(data);
             });
             stream.on('end', function () {
@@ -31,11 +32,11 @@ tape ('1.A create echo server', function (t) {
                 t.equal(''+data, 'test');
                 ws.end(); // TODO on(end)
             });
-            ws.write('test', function () {
-                t.pass('sent');
-            });
             ws.on('end', function () {
                 t.pass('client stream ends');
+            });
+            ws.write('test', function () {
+                t.pass('sent');
             });
         });
     });
